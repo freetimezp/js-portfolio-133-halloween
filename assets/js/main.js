74218caa463ae76592c924f6ca8b3a319fb4662f
freetimezp <kraftukrnet@ukr.net>
2024-10-25 20:51:56 +0300

    02. js-portfolio-133-halloween. Menu. Toggle.

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+//show menu
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+
+//remove menu mobile
+const navLinks = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+};
+
+navLinks.forEach(n => n.addEventListener('click', linkAction));
+
+
+
+
+
+
+
+
+
+
+
+
+
